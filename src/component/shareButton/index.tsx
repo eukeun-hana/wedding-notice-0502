89aabcd2ae313b9257f9ce48f6157b6dc2774c62
eukeun-hana <connetflix@naver.com>
2026-03-16 21:30:12 +0900

    Comment out wedding date in shareButton component
    Comment out wedding date formatting in share button.

--- src/component/shareButton/index.tsx
+++ src/component/shareButton/index.tsx
@@ -26,8 +26,8 @@ export const ShareButton = () => {
             content: {
               title: `${GROOM_FULLNAME} ❤️ ${BRIDE_FULLNAME} 결혼합니다.`,
               description:
-                WEDDING_DATE.format(WEDDING_DATE_FORMAT),
-                
+                //WEDDING_DATE.format(WEDDING_DATE_FORMAT),
+                "",
               imageUrl:
                 window.location.protocol +
                 "//" +

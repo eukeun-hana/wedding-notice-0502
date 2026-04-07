eccb0512b3670077a466c9c0d184e4d57e032ec6
eukeun-hana <connetflix@naver.com>
2026-04-07 22:21:13 +0900

    Update provider.tsx

--- src/component/store/provider.tsx
+++ src/component/store/provider.tsx
@@ -4,10 +4,11 @@ import { PropsWithChildren, useState } from "react"
 import { StoreContext } from "./context"
 
 export const StoreProvider = ({ children }: PropsWithChildren) => {
+  const [naver, setNaver] = useState<any>(null)
   const [kakao, setKakao] = useState<any>(null)
 
   return (
-    <StoreContext.Provider value={{ kakao, setKakao }}>
+    <StoreContext.Provider value={{ naver, setNaver, kakao, setKakao }}>
       {children}
     </StoreContext.Provider>
   )
